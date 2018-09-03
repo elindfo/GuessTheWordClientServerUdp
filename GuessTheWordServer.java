26cfb576381f7893fbbdbb07f62a8844d85fd6a0
elindfo <elindfo@kth.se>
2018-09-03 17:21:05 +0200

    Add command line argument check

--- GuessTheWordServer.java
+++ GuessTheWordServer.java
@@ -3,6 +3,12 @@ package lab1a;
 public class GuessTheWordServer {
 
     public static void main(String[] args) {
-        
+        if(args.length != 1){
+            System.out.println("You need to enter a word.");
+            System.out.println("Usage: java guessthewordserver [word]");
+            System.exit(1);
+        }
+
+        System.out.println("Word set to: " + args[0]);
     }
 }
